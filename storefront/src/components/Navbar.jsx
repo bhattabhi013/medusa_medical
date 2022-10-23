05e03e58dce3b064c8faf31cdc9bea7cbfa610a2
Vanshita Mathur <vanshitamathur2307@gmail.com>
2022-10-24 00:46:40 +0530

    Update Navbar.jsx

--- storefront/src/components/Navbar.jsx
+++ storefront/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
     <div>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <NavLink className="navbar-brand" to="/Home">MediSwiss</NavLink>
+    <NavLink className="navbar-brand" to="/Home">SwagMission</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
