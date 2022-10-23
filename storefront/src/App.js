20d5449b71a20c188d721f7cdf6e2e996aea2ff3
zugzwang03 <2025.cse63.shreyasarkar@gmail.com>
2022-10-23 15:08:58 +0530

    about page done

--- storefront/src/App.js
+++ storefront/src/App.js
@@ -5,6 +5,7 @@ import Products from './components/Products';
 import Home from './components/Home';
 import {Routes, Route} from 'react-router-dom'
 import Checkout from './components/Checkout';
+import About from './components/About';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
 <Route exact path="/Home" element={<Home/>} />
 <Route exact path="/Products" element={<Products/>} />
 <Route exact path="/Checkout" element={<Checkout/>}/>
+<Route exact path="/about" element={<About/>}/>
 
       </Routes>
      
--- storefront/src/components/About.js
+++ storefront/src/components/About.js
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <React.Fragment>
+    <h1>About Us</h1>
+    <p><b>Shoppe</b> is the online medical store, we all need. It offers medicines at affordable price and at remote locations.
+    Our delivery service is quite trustworthy, efficient and smooth. Nowadays ecommerce websites mostly focus on clothes,stationaries,and daily household chores, but
+    we tend to make the medical world easier and accessible by customizing the medicine availability in all remote locations irrespective of any city or region barrier.
+    We have various services available here including medicine classifications, disease based segregation, smooth processing of payment through cash or card, easy
+    checkout policies, and to add the cherry on the cake, a reliable return policy as well in case of any predicament or faulty product delivery. Our services are available at an unstoppable 
+    pace and on a regular basis. Now our customers do not have to worry about ailments, and chronic diseases, as they could recuperate themselves in the wink of an eye by accessing our medicines. </p>
+    <footer>&copy; 2022 <b>SHOPPE</b></footer>
+    </React.Fragment>
   )
 }
